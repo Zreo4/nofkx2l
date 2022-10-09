9635b7febebeb1af1063f5ba1b9f9d6ba17c33fa
Zreo4 <40686787+Zreo4@users.noreply.github.com>
2022-10-09 22:56:32 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 PORT=80
-UUID=5aaed9b7-7fe3-47c3-bb52-db59859ce198
+UUID=ff9b8c96-5caf-42ae-b071-1925dc2ad185
 WebPage=https://bing.com
 CaddyConfig=https://raw.githubusercontent.com/bsefwe/Rail/main/etc/Caddyfile
 XRayConfig=https://raw.githubusercontent.com/bsefwe/Rail/main/etc/xray.json
